74cad7c1831c52ad4b6e59c226d340d50783f145
leandroSiq1 <le_kts12@hotmail.com>
2021-06-22 11:17:51 -0300

    finalizando aula 2

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,23 +1,20 @@
-import { useContext } from 'react';
-import { AuthContext } from '../App';
-
 import { Link } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 export function NewRoom() {
-  const { user } = useContext(AuthContext);
+  // const { user } = useAuth();
 
   return (
     <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />        
-        {/* <h1>{user?.name}</h1> */}
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas de sua audiência em tempo-real</p>
       </aside>
@@ -25,7 +22,6 @@ export function NewRoom() {
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
           <h2>Cria uma nova sala</h2>
-
           <form>
             <input 
               type="text"

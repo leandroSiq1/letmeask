a2899b281b7b32218fec45960f1fb1bc331bc889
leandroSiq1 <le_kts12@hotmail.com>
2021-06-24 13:12:52 -0300

    excluindo pergunta; encerrando sala; e função de like

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-
+import { Room } from "./pages/Room";
+import { AdminRoom } from './pages/AdminRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
@@ -10,8 +11,13 @@ export default function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom}></Route>
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
